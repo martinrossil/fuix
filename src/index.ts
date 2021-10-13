dcbf67d63c6961d04fecd52c6f7b2c125a78c376
Martin Rossil <martinrossil@gmail.com>
2021-10-13 22:39:42 +0200

    Added files to index.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
 export const VERSION = '0.0.1';
+export { Position } from './consts/Position';
+export { default as BaseElement } from './core/BaseElement';
+export { default as IPositionElement } from './core/IPositionElement';
+export { default as PositionElement } from './core/PositionElement';
+export { PositionType } from './types/PositionType';
