decf09d67a04f1b03f8668d904865d7dca681c6e
Martin Rossil <martinrossil@gmail.com>
2021-10-14 19:33:53 +0200

    Added documentation.

--- src/core/IPositionElement.ts
+++ src/core/IPositionElement.ts
@@ -1,9 +1,24 @@
 import { PositionType } from '../types/PositionType';
 
 export default interface IPositionElement {
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/position} for syntax
+     */
     position: PositionType;
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/left} for syntax
+     */
     left: number;
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/top} for syntax
+     */
     top: number;
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/right} for syntax
+     */
     right: number;
+    /**
+     * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/bottom} for syntax
+     */
     bottom: number;
 }
--- src/types/PositionType.ts
+++ src/types/PositionType.ts
@@ -1,2 +1,8 @@
-// https://developer.mozilla.org/en-US/docs/Web/CSS/position
-export type PositionType = '' | 'static' | 'relative' | 'absolute' | 'fixed' | 'sticky';
+/**
+ * The position CSS property sets how an element is positioned in a document.
+ * The top, right, bottom, and left properties determine the final location of positioned elements.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/position} for syntax
+ *
+ */
+export type PositionType = 'static' | 'relative' | 'absolute' | 'fixed' | 'sticky';
