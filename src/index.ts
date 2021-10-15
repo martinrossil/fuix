dd175c63deafb71d7f48af416e2b85239a366c3a
Martin Rossil <martinrossil@gmail.com>
2021-10-15 20:51:05 +0200

    Added SizeElement and Interface.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 export { Position } from './consts/Position';
 export { default as IPositionElement } from './core/IPositionElement';
+export { default as ISizeElement } from './core/ISizeElement';
 export { default as PositionElement } from './core/PositionElement';
+export { default as SizeElement } from './core/SizeElement';
 export { PositionType } from './types/PositionType';
