7527feb71acbcd71449d35f55cb3b5762f81c40f
Martin Rossil <martinrossil@gmail.com>
2021-10-15 20:48:40 +0200

    Removed line breaks.

--- src/types/PositionType.ts
+++ src/types/PositionType.ts
@@ -1,8 +1,6 @@
 /**
- * The position CSS property sets how an element is positioned in a document.
- * The top, right, bottom, and left properties determine the final location of positioned elements.
+ * The position CSS property sets how an element is positioned in a document. The top, right, bottom, and left properties determine the final location of positioned elements.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/position} for syntax
- *
  */
 export type PositionType = 'static' | 'relative' | 'absolute' | 'fixed' | 'sticky';
