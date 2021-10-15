6a7cbf24248dcde066de4a6a8e3f53798482f198
Martin Rossil <martinrossil@gmail.com>
2021-10-15 20:49:07 +0200

    Added comments.

--- src/core/IPositionElement.ts
+++ src/core/IPositionElement.ts
@@ -2,22 +2,32 @@ import { PositionType } from '../types/PositionType';
 
 export default interface IPositionElement {
     /**
+     * The position CSS property sets how an element is positioned in a document. The top, right, bottom, and left properties determine the final location of positioned elements.
+     *
      * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/position} for syntax
      */
     position: PositionType;
     /**
+     * The left CSS property participates in specifying the horizontal position of a positioned element. It has no effect on non-positioned elements.
+     *
      * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/left} for syntax
      */
     left: number;
     /**
+     * The top CSS property participates in specifying the vertical position of a positioned element. It has no effect on non-positioned elements.
+     *
      * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/top} for syntax
      */
     top: number;
     /**
+     * The right CSS property participates in specifying the horizontal position of a positioned element. It has no effect on non-positioned elements.
+     *
      * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/right} for syntax
      */
     right: number;
     /**
+     * The bottom CSS property participates in setting the vertical position of a positioned element. It has no effect on non-positioned elements.
+     *
      * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/bottom} for syntax
      */
     bottom: number;
