b292c9ffb351e47eb89d4673d566640e6ef05e98
Martin Rossil <martinrossil@gmail.com>
2021-10-18 21:54:17 +0200

    Updated index.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+export { Display } from './consts/Display';
 export { Position } from './consts/Position';
+
 export { default as IPositionElement } from './core/IPositionElement';
 export { default as ISizeElement } from './core/ISizeElement';
 export { default as PositionElement } from './core/PositionElement';
 export { default as SizeElement } from './core/SizeElement';
+export { DisplayType } from './types/DisplayType';
 export { PositionType } from './types/PositionType';
