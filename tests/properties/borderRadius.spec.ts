93a6f56b290ef06534091cb07cf9d1eb9be2d4f2
Martin Rossil <martinrossil@gmail.com>
2021-10-20 21:40:51 +0200

    Changed borderRadius test.

--- tests/properties/borderRadius.spec.ts
+++ tests/properties/borderRadius.spec.ts
@@ -33,27 +33,26 @@ describe('IDisplayElement.borderRadius property', () => {
             displayElement.borderRadius = minusRandom;
             assert.strictEqual(displayElement.borderRadius, 0);
         });
-
         it('given borderRadius is 0, when borderRadius = ' + positiveRandom + ', style.borderRadius should be ' + positiveRandom + 'px', () => {
             const displayElement: DisplayElement = new DisplayElement();
             displayElement.borderRadius = positiveRandom;
             assert.strictEqual(displayElement.style.borderRadius, positiveRandom + 'px');
         });
-        it('given borderRadius is 0, when borderRadius = 0, style.borderRadius should be 0px', () => {
+        it('given borderRadius is 0, when borderRadius = 0, style.borderRadius should be ""', () => {
             const displayElement: DisplayElement = new DisplayElement();
             displayElement.borderRadius = 0;
-            assert.strictEqual(displayElement.style.borderRadius, '0px');
+            assert.strictEqual(displayElement.style.borderRadius, '');
         });
-        it('given borderRadius is 10, when borderRadius = NaN, style.borderRadius should be 0px', () => {
+        it('given borderRadius is 10, when borderRadius = NaN, style.borderRadius should be ""', () => {
             const displayElement: DisplayElement = new DisplayElement();
             displayElement.borderRadius = 10;
             displayElement.borderRadius = NaN;
-            assert.strictEqual(displayElement.style.borderRadius, '0px');
+            assert.strictEqual(displayElement.style.borderRadius, '');
         });
-        it('given borderRadius is 0, when borderRadius = ' + minusRandom + ', style.borderRadius should be 0', () => {
+        it('given borderRadius is 0, when borderRadius = ' + minusRandom + ', style.borderRadius should be ""', () => {
             const displayElement: DisplayElement = new DisplayElement();
             displayElement.borderRadius = minusRandom;
-            assert.strictEqual(displayElement.style.borderRadius, '0px');
+            assert.strictEqual(displayElement.style.borderRadius, '');
         });
     });
 });
