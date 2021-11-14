79d4fb081e4bf6416f9975ced7f048b62c6de299
Martin Rossil <martinrossil@gmail.com>
2021-11-14 16:46:25 +0100

    Testing IApplicationElement.

--- src/FuixDev.ts
+++ src/FuixDev.ts
@@ -4,6 +4,9 @@ export default class FuixDev extends ApplicationElement {
     public constructor() {
         super();
         console.log('FuixDev');
+        this.bodyBackgroundColor = '#000d1a';
+        this.bodyColor = '#a3b8cc';
+        this.bodyFontFamily = 'Eurostile';
     }
 }
 customElements.define('fuix-dev', FuixDev);
