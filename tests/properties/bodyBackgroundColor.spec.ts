ed6b9ea8f294c78b615a680aa64596d1de0d7f22
Martin Rossil <martinrossil@gmail.com>
2021-11-17 22:38:12 +0100

    Fixed misspelling.

--- tests/properties/bodyBackgroundColor.spec.ts
+++ tests/properties/bodyBackgroundColor.spec.ts
@@ -1,22 +1,22 @@
 import { assert } from 'chai';
 import { describe, it } from 'mocha';
-import { AppliationElement, IAppliationElement } from '../../src/';
+import { ApplicationElement, IAppliationElement } from '../../src';
 
 describe('IApplicationElement.bodyBackgroundColor property', () => {
     describe('default value', () => {
         it('bodyBackgroundColor should be ""', () => {
-            const applicationElement: IAppliationElement = new AppliationElement();
+            const applicationElement: IAppliationElement = new ApplicationElement();
             assert.strictEqual(applicationElement.bodyBackgroundColor, '');
         });
     });
     describe('bodyBackgroundColor property', () => {
         it('given bodyBackgroundColor is "", when bodyBackgroundColor = "rgb(18, 52, 86)", bodyBackgroundColor should be "rgb(18, 52, 86)"', () => {
-            const applicationElement: IAppliationElement = new AppliationElement();
+            const applicationElement: IAppliationElement = new ApplicationElement();
             applicationElement.bodyBackgroundColor = 'rgb(18, 52, 86)';
             assert.strictEqual(applicationElement.bodyBackgroundColor, 'rgb(18, 52, 86)');
         });
         it('given bodyBackgroundColor is "rgb(18, 52, 86)", when bodyBackgroundColor = "", bodyBackgroundColor should be ""', () => {
-            const applicationElement: IAppliationElement = new AppliationElement();
+            const applicationElement: IAppliationElement = new ApplicationElement();
             applicationElement.bodyBackgroundColor = 'rgb(18, 52, 86)';
             applicationElement.bodyBackgroundColor = '';
             assert.strictEqual(applicationElement.bodyBackgroundColor, '');
