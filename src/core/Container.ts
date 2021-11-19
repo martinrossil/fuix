f1105ed284403ec677c4f2a9e14bf218b950992c
Martin Rossil <martinrossil@gmail.com>
2021-11-19 19:55:37 +0100

    Added methods for child manipulation.

--- src/core/Container.ts
+++ src/core/Container.ts
@@ -1,3 +1,4 @@
+import IComponent from './IComponent';
 import Component from './Component';
 import IContainer from './IContainer';
 
@@ -20,6 +21,38 @@ export default class Container extends Component implements IContainer {
      */
     public get padding(): number {
         return this._padding;
+    }
+
+    /**
+     * The addComponent() method of the IContainer interface adds a IComponent to the end of the list of children of a specified parent IContainer. If the given IComponent is a reference to an existing IComponent in the document, addComponent() moves it from its current position to the new position (there is no requirement to remove the IComponent from its parent IContainer before appending it to some other IContainer).
+     *
+     * @param component - the component to add as child node.
+     * @returns the calling component so we can chain.
+     */
+    public addComponent(component: IComponent): this {
+        this.appendChild(component as unknown as Node);
+        return this;
+    }
+
+    /**
+     * The removeComponent() method of the IContainer interface removes a Component from the DOM.
+     *
+     * @param component - the component to remove as child node.
+     * @returns the calling component so we can chain.
+     */
+    public removeComponent(component: IComponent): this {
+        this.removeChild(component as unknown as Node);
+        return this;
+    }
+
+    /**
+     * The containsComponent() method of the IContainer interface returns a boolean value indicating whether a IComponent is a descendant of a given IContainer, that is the IContainer itself, one of its direct children (IComponents), one of the children's direct children, and so on.
+     *
+     * @param component - The IComponent to test with.
+     * @returns A boolean value that is true if component is contained in the IContainer, false if not.
+     */
+    public containsComponent(component: IComponent): boolean {
+        return this.contains(component as unknown as Node);
     }
 }
 customElements.define('fx-container', Container);
--- src/core/IContainer.ts
+++ src/core/IContainer.ts
@@ -1,10 +1,34 @@
 import IComponent from './IComponent';
 
-export default interface IDisplayContainer extends IComponent {
+export default interface IContainer extends IComponent {
     /**
      * The padding CSS shorthand property sets the padding area on all four sides of an element at once.
      *
      * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/padding} for syntax
      */
     padding: number;
+
+    /**
+     * The addComponent() method of the IContainer interface adds a IComponent to the end of the list of children of a specified parent IContainer. If the given IComponent is a reference to an existing IComponent in the document, addComponent() moves it from its current position to the new position (there is no requirement to remove the IComponent from its parent IContainer before appending it to some other IContainer).
+     *
+     * @param component - the component to add as child node.
+     * @returns the calling component so we can chain.
+     */
+    addComponent(component: IComponent): this;
+
+    /**
+     * The removeComponent() method of the IContainer interface removes a Component from the DOM.
+     *
+     * @param component - the component to remove as child node.
+     * @returns the calling component so we can chain.
+     */
+    removeComponent(component: IComponent): this;
+
+    /**
+     * The containsComponent() method of the IContainer interface returns a boolean value indicating whether a IComponent is a descendant of a given IContainer, that is the IContainer itself, one of its direct children (IComponents), one of the children's direct children, and so on.
+     *
+     * @param component - The IComponent to test with.
+     * @returns A boolean value that is true if component is contained in the IContainer, false if not.
+     */
+    containsComponent(component: IComponent): boolean;
 }
