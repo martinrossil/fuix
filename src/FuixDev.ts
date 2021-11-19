46e0ac39abe91ccb56ebb77cf520fe2470b83f43
Martin Rossil <martinrossil@gmail.com>
2021-11-19 21:49:12 +0100

    Testing github.dev

--- src/FuixDev.ts
+++ src/FuixDev.ts
@@ -1,4 +1,6 @@
 import Application from './core/Application';
+import IComponent from './core/IComponent';
+import Component from './core/Component';
 
 export default class FuixDev extends Application {
     public constructor() {
@@ -7,6 +9,7 @@ export default class FuixDev extends Application {
         this.bodyBackgroundColor = '#000d1a';
         this.bodyColor = '#a3b8cc';
         this.bodyFontFamily = 'Eurostile';
+        const component: IComponent = new Component();
     }
 }
 customElements.define('fuix-dev', FuixDev);
