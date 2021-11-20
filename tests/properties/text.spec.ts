51db58d4e932ec50b8bf049e0bd4a05bb2547f4d
Martin Rossil <martinrossil@gmail.com>
2021-11-20 23:17:22 +0100

    Created text property test.

---
+++ tests/properties/text.spec.ts
@@ -0,0 +1,24 @@
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+import Text from '../../src/core/Text';
+import IText from '../../src/core/IText';
+
+describe('IText.text property', () => {
+    describe('default value', () => {
+        it('text should be ""', () => {
+            const text: IText = new Text();
+            assert.strictEqual(text.text, '');
+        });
+    });
+    it('given text is "", when text = "Lorem ipsum", text should be "Lorem ipsum"', () => {
+        const text: IText = new Text();
+        text.text = 'Lorem ipsum';
+        assert.strictEqual(text.text, 'Lorem ipsum');
+    });
+    it('given text is "Lorem ipsum", when text = "", text should be ""', () => {
+        const text: IText = new Text();
+        text.text = 'Lorem ipsum';
+        text.text = '';
+        assert.strictEqual(text.text, '');
+    });
+});
