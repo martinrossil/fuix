0c11a0804acff9d3f591b8d26305b138ce5f04b7
Martin Rossil <martinrossil@gmail.com>
2021-11-23 20:47:25 +0100

    Added FontWeightType.

---
+++ src/types/FontWeightType.ts
@@ -0,0 +1,12 @@
+import { FontWeight } from '../consts/FontWeight';
+
+export type FontWeightType = FontWeight.THIN_100 |
+                            FontWeight.EXTRA_LIGHT_200 |
+                            FontWeight.LIGHT_300 |
+                            FontWeight.NORMAL_400 |
+                            FontWeight.MEDIUM_500 |
+                            FontWeight.SEMI_BOLD_600 |
+                            FontWeight.BOLD_700 |
+                            FontWeight.EXTRA_BOLD_800 |
+                            FontWeight.BLACK_900 |
+                            FontWeight.EXTRA_BLACK_950;
