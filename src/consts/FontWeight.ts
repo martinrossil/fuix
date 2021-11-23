4a7136653ea3e750914f21d413deef21c56c6a33
Martin Rossil <martinrossil@gmail.com>
2021-11-23 20:45:15 +0100

    Added FontWeight const enums.

---
+++ src/consts/FontWeight.ts
@@ -0,0 +1,12 @@
+export const enum FontWeight {
+    THIN_100 = 100,
+    EXTRA_LIGHT_200 = 200,
+    LIGHT_300 = 300,
+    NORMAL_400 = 400,
+    MEDIUM_500 = 500,
+    SEMI_BOLD_600 = 600,
+    BOLD_700 = 700,
+    EXTRA_BOLD_800 = 800,
+    BLACK_900 = 900,
+    EXTRA_BLACK_950 = 950
+}
