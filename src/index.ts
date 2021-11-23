f8afe9ab08ea0be922c52edc14c87b15afc35a17
Martin Rossil <martinrossil@gmail.com>
2021-11-23 20:48:02 +0100

    Updated index.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 export { Display } from './consts/Display';
+export { FontWeight } from './consts/FontWeight';
 export { Overflow } from './consts/Overflow';
 export { Position } from './consts/Position';
 
 export { default as Application } from './core/Application';
-export { default as Container } from './core/Container';
 export { default as Component } from './core/Component';
+export { default as Container } from './core/Container';
 export { default as IApplication } from './core/IApplication';
-export { default as IContainer } from './core/IContainer';
 export { default as IComponent } from './core/IComponent';
+export { default as IContainer } from './core/IContainer';
+export { default as IText } from './core/IText';
+export { default as Text } from './core/Text';
 
 export { DisplayType } from './types/DisplayType';
+export { FontWeightType } from './types/FontWeightType';
 export { OverflowType } from './types/OverflowType';
 export { PositionType } from './types/PositionType';
