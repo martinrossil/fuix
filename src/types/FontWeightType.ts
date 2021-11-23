e879a044c574ec836433dcc1baa29c9a5a24d975
Martin Rossil <martinrossil@gmail.com>
2021-11-23 22:49:11 +0100

    Changed to numeric values.

--- src/types/FontWeightType.ts
+++ src/types/FontWeightType.ts
@@ -1,12 +1,3 @@
 import { FontWeight } from '../consts/FontWeight';
 
-export type FontWeightType = FontWeight.THIN_100 |
-                            FontWeight.EXTRA_LIGHT_200 |
-                            FontWeight.LIGHT_300 |
-                            FontWeight.NORMAL_400 |
-                            FontWeight.MEDIUM_500 |
-                            FontWeight.SEMI_BOLD_600 |
-                            FontWeight.BOLD_700 |
-                            FontWeight.EXTRA_BOLD_800 |
-                            FontWeight.BLACK_900 |
-                            FontWeight.EXTRA_BLACK_950;
+export type FontWeightType = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950;
