e4845be672acb9f83fb869a670bf891f156c5926
Martin Rossil <martinrossil@gmail.com>
2021-11-23 22:56:55 +0100

    Added missing comments.

--- src/types/FontWeightType.ts
+++ src/types/FontWeightType.ts
@@ -1,3 +1,6 @@
-import { FontWeight } from '../consts/FontWeight';
-
+/**
+ * The font-weight CSS property sets the weight (or boldness) of the font. The weights available depend on the font-family that is currently set.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/font-weight} for syntax
+*/
 export type FontWeightType = 100 | 200 | 300 | 400 | 500 | 600 | 700 | 800 | 900 | 950;
