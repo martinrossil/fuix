36eb399622d859137afa6f6d5f5f853a2068b935
Martin Rossil <martinrossil@gmail.com>
2021-11-24 22:19:35 +0100

    Removed global font-family style.

--- src/core/Application.ts
+++ src/core/Application.ts
@@ -17,7 +17,6 @@ export default class Application extends Container implements IApplication {
         innerHTMLString += 'margin: 0; line-height: 1.2;';
         innerHTMLString += 'position: relative; box-sizing:border-box;';
         innerHTMLString += 'border:none; outline:none;';
-        innerHTMLString += 'font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";';
         innerHTMLString += '}';
         const style: HTMLStyleElement = document.createElement('style');
         style.innerHTML = innerHTMLString;
