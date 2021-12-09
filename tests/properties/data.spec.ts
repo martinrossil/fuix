073fca8f0c91f21f74f72a7f977c107aadf201cb
Martin Rossil <martinrossil@gmail.com>
2021-12-09 21:54:42 +0100

    Created data property test.

---
+++ tests/properties/data.spec.ts
@@ -0,0 +1,26 @@
+import { assert } from 'chai';
+import { describe, it } from 'mocha';
+import { DataRenderer, IDataRenderer } from '../../src';
+const positiveRandom = Math.round(Math.random() * 1000);
+
+describe('IDataRenderer.data property', () => {
+    describe('default value', () => {
+        it('data should be null', () => {
+            const dataRenderer: IDataRenderer<unknown> = new DataRenderer();
+            assert.isNull(dataRenderer.data);
+        });
+    });
+    describe('data property', () => {
+        it('given data is null, when data = ' + positiveRandom + ', data should be ' + positiveRandom, () => {
+            const dataRenderer: IDataRenderer<number> = new DataRenderer();
+            dataRenderer.data = positiveRandom;
+            assert.strictEqual(dataRenderer.data, positiveRandom);
+        });
+        it('given data is ' + positiveRandom + ', when data = ' + positiveRandom + ', data should be ' + positiveRandom, () => {
+            const dataRenderer: IDataRenderer<number> = new DataRenderer();
+            dataRenderer.data = positiveRandom;
+            dataRenderer.data = positiveRandom;
+            assert.strictEqual(dataRenderer.data, positiveRandom);
+        });
+    });
+});
