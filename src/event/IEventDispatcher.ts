f0f187f197f57f896f7f8e9221f3465b6a1c690c
Martin Rossil <martinrossil@gmail.com>
2022-03-03 14:04:25 +0100

    Added init object to listeners.

--- src/event/IEventDispatcher.ts
+++ src/event/IEventDispatcher.ts
@@ -3,9 +3,9 @@ import ICustomEventListener from './ICustomEventListener';
 
 export default interface IEventDispatcher {
     dispatchEvent(event: Event): boolean;
-    addEventListener(type: string, listener: IEventListener): void;
+    addEventListener(type: string, listener: IEventListener, options?: { passive: boolean }): void;
     removeEventListener(type: string, listener: IEventListener): void;
     dispatchCustomEvent(event: CustomEvent): boolean;
-    addCustomEventListener(type: string, customListener: ICustomEventListener): void;
+    addCustomEventListener(type: string, customListener: ICustomEventListener, options?: { passive: boolean }): void;
     removeCustomEventListener(type: string, customListener: ICustomEventListener): void;
 }
