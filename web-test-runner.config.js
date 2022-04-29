93e9044bc8d5ee1bd2c6155dc094871230410fc4
Martin Rossil <martinrossil@gmail.com>
2022-04-29 08:53:02 +0200

    Installed test runner.

---
+++ web-test-runner.config.js
@@ -0,0 +1,22 @@
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
+export default {
+    files: ['src/**/*.test.ts'],
+    plugins: [esbuildPlugin({ ts: true })],
+    browsers: [
+        playwrightLauncher({ product: 'chromium' }),
+        playwrightLauncher({ product: 'firefox' }),
+        playwrightLauncher({ product: 'webkit' })
+    ],
+    coverageConfig: {
+        report: true,
+        reportDir: 'coverage',
+        threshold: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100
+        }
+    }
+};
