30cdaaec4e9b2e7488129fda878c5530b1254f47
Martin Rossil <martinrossil@gmail.com>
2022-05-06 15:37:57 +0200

    Created IDataRenderer test.

---
+++ src/components/IDataRenderer.test.ts
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+import { assert } from 'chai';
+import DataRenderer from './DataRenderer';
+import IDataRenderer from './IDataRenderer';
+
+const positiveRandom = Math.round(Math.random() * 1000);
+
+describe('IDataRenderer.data property', () => {
+    describe('default value', () => {
+        it('data should be null', () => {
+            const dataRenderer: IDataRenderer<unknown> = new DataRenderer();
+            assert.isNull(dataRenderer.data);
+        });
+    });
+    describe('data property', () => {
+        it('given data is null, when data = ' + positiveRandom + ', data should be ' + positiveRandom, () => {
+            const dataRenderer: IDataRenderer<number> = new DataRenderer();
+            dataRenderer.data = positiveRandom;
+            assert.strictEqual(dataRenderer.data, positiveRandom);
+        });
+        it('given data is ' + positiveRandom + ', when data = ' + positiveRandom + ', data should be ' + positiveRandom, () => {
+            const dataRenderer: IDataRenderer<number> = new DataRenderer();
+            dataRenderer.data = positiveRandom;
+            dataRenderer.data = positiveRandom;
+            assert.strictEqual(dataRenderer.data, positiveRandom);
+        });
+    });
+});
