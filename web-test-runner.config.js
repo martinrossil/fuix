120e0fdc50e089a7413b15a216a6578df395d0fc
Martin Rossil <martinrossil@gmail.com>
2022-05-09 09:29:16 +0200

    Added reporters.

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,12 +1,20 @@
 import { esbuildPlugin } from '@web/dev-server-esbuild';
 import { playwrightLauncher } from '@web/test-runner-playwright';
+import { defaultReporter, summaryReporter } from '@web/test-runner';
 
 export default {
     files: ['src/**/*.test.ts'],
     plugins: [esbuildPlugin({ ts: true })],
     browsers: [
-        playwrightLauncher({ product: 'chromium' }),
-        playwrightLauncher({ product: 'firefox' })
+        playwrightLauncher({ product: 'chromium' })//,
+        // playwrightLauncher({ product: 'firefox' })
+    ],
+    reporters: [
+        summaryReporter(),
+        defaultReporter({
+            reportTestResults: true,
+            reportTestProgress: true
+        })
     ],
     coverageConfig: {
         report: true,
