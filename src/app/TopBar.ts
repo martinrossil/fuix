884fd5f2cbab4f92e85ec0025becdff062658241
Martin Rossil <martinrossil@gmail.com>
2022-05-09 13:10:59 +0200

    Created TopBar.

---
+++ src/app/TopBar.ts
@@ -0,0 +1,14 @@
+import { Position } from '../consts/Position';
+import Container from '../core/Container';
+
+export default class TopBar extends Container {
+    public constructor() {
+        super();
+        this.position = Position.FIXED;
+        this.backgroundColor = '#333366';
+        this.height = 64;
+        this.widthPercent = 100;
+        this.top = 0;
+    }
+}
+customElements.define('top-bar', TopBar);
