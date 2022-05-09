6de4ff9e3a893050db37ae31cb1dde1eec732785
Martin Rossil <martinrossil@gmail.com>
2022-05-09 13:11:08 +0200

    Created BottomBar.

---
+++ src/app/BottomBar.ts
@@ -0,0 +1,15 @@
+import { Display } from '../consts/Display';
+import { Position } from '../consts/Position';
+import Container from '../core/Container';
+
+export default class BottomBar extends Container {
+    public constructor() {
+        super();
+        this.display = Display.BLOCK;
+        this.position = Position.STICKY;
+        this.backgroundColor = '#333366';
+        this.height = 64;
+        this.bottom = 0;
+    }
+}
+customElements.define('bottom-bar', BottomBar);
