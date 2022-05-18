6016191bc62a9f9383a3f69a7057d7ef98db8e11
Martin Rossil <martinrossil@gmail.com>
2022-05-18 12:17:03 +0200

    Refactored to use const instead of enum.

--- src/types/DisplayType.ts
+++ src/types/DisplayType.ts
@@ -1,8 +1,15 @@
 import { Display } from '../consts/Display';
-
 /**
  * The display CSS property sets whether an element is treated as a block or inline element and the layout used for its children, such as flow layout, grid or flex. Formally, the display property sets an element's inner and outer display types. The outer type sets an element's participation in flow layout; the inner type sets the layout of children. Some values of display are fully defined in their own individual specifications; for example the detail of what happens when display: flex is declared is defined in the CSS Flexible Box Model specification.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/display} for syntax
  */
-export type DisplayType = Display.BLOCK | Display.INLINE | Display.FLEX | Display.GRID | Display.NONE;
+export type DisplayType =
+    typeof Display.BLOCK |
+    typeof Display.INLINE |
+    typeof Display.INLINE_BLOCK |
+    typeof Display.INLINE_FLEX |
+    typeof Display.FLEX |
+    typeof Display.INLINE_GRID |
+    typeof Display.GRID |
+    typeof Display.NONE;
