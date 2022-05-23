73a7249fc5c8e749c64127a99c5d7993ede77238
Martin Rossil <martinrossil@gmail.com>
2022-05-23 13:12:31 +0200

    Refactored to use static getters.

--- src/types/FlexWrapType.ts
+++ src/types/FlexWrapType.ts
@@ -1,8 +1,10 @@
-import { FlexWrap } from '../consts/FlexWrap';
+import FlexWrap from '../consts/FlexWrap';
 
 /**
  * The flex-wrap CSS property sets whether flex items are forced onto one line or can wrap onto multiple lines. If wrapping is allowed, it sets the direction that lines are stacked.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/flex-wrap} for syntax
+ *
+ * @beta
  */
-export type FlexWrapType = FlexWrap.NOWRAP | FlexWrap.WRAP | FlexWrap.WRAP_REVERSE;
+export type FlexWrapType = typeof FlexWrap.NOWRAP | typeof FlexWrap.WRAP | typeof FlexWrap.WRAP_REVERSE; // 'nowrap' | 'wrap' | 'wrap-reverse'; // FlexWrap.NOWRAP | FlexWrap.WRAP | FlexWrap.WRAP_REVERSE;
