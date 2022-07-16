edb68db28f85ad3a96dd009673202814dad7047d
Martin Rossil <martinrossil@gmail.com>
2022-07-16 15:59:34 +0200

    Deleted test directory.

--- src/test/ITestComponent.ts
+++
@@ -1,7 +0,0 @@
-import IComponent from '../core/IComponent';
-
-export default interface ITestComponent extends IComponent {
-    test(input: string): string;
-    twenty: number;
-    nums: 'one' | 'two';
-}
--- src/test/TestComponentEvent.ts
+++
@@ -1,8 +0,0 @@
-export default class TestComponentEvent extends Event {
-    public static TEST = 'test';
-    public prop: string;
-    public constructor(type: string, prop = '', bubbles = false) {
-        super(type, { bubbles });
-        this.prop = prop;
-    }
-}
