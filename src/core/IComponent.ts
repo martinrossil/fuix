8d4f0815d8d79c23c171fcb2a13b79b0e414d56b
Martin Rossil <martinrossil@gmail.com>
2022-07-17 13:47:31 +0200

    Fixes #7

--- src/core/IComponent.ts
+++ src/core/IComponent.ts
@@ -144,4 +144,8 @@ export default interface IComponent {
     /**
      * The computedStyle() method returns an object containing the values of all CSS properties of the Component, after applying active stylesheets and resolving any basic computation those values may contain. Individual CSS property values are accessed through APIs provided by the object, or by indexing with CSS property names.
      */
+
+    /**
+     *
+     */
 }
