a05187c5caf885412a6949603d2e78c5da6c2e45
Martin Rossil <martinrossil@gmail.com>
2022-08-01 10:26:49 +0200

    Created TextOverflow.

---
+++ src/types/TextOverflow.ts
@@ -0,0 +1,6 @@
+/**
+ * The text-overflow CSS property sets how hidden overflow content is signaled to users. It can be clipped, display an ellipsis ('…'), or display a custom string.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/CSS/text-overflow}
+ */
+export type TextOverflow = 'clip' | 'ellipsis';
