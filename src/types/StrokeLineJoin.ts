8cba5f3d841e26d5827483815885a0ab7a73b6b4
Martin Rossil <martinrossil@gmail.com>
2022-08-07 19:14:41 +0200

    Created StrokeLineJoin.

---
+++ src/types/StrokeLineJoin.ts
@@ -0,0 +1,7 @@
+/**
+ * The stroke-linejoin attribute is a presentation attribute defining the shape to be used at the corners of paths when they are stroked.
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/stroke-linejoin} for syntax
+ */
+
+export type StrokeLineJoin = 'arcs' | 'bevel' | 'miter' | 'miter-clip' | 'round';
