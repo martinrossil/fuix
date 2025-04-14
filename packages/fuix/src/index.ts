4b1c9047d73c4775bd5eab8d957cf2e7cd74baee
martinrossil <martinrossil@gmail.com>
2025-04-14 12:04:41 +0200

    feat: Create Stroke class closes #47

--- packages/fuix/src/index.ts
+++ packages/fuix/src/index.ts
@@ -2,3 +2,4 @@ export { default as Component } from './component';
 export { default as Hex } from './hex';
 export { default as LinearGradient } from './linear-gradient';
 export { default as StopColor } from './stop-color';
+export { default as Stroke } from './stroke';
---
+++ packages/fuix/src/stroke.ts
@@ -0,0 +1,82 @@
+import Hex from './hex';
+
+export default class Stroke {
+	#color: Hex;
+	#position: 'center' | 'inside' | 'outside';
+	#style: 'dash' | 'solid';
+	#weight: number;
+
+	/**
+	 * The outline CSS shorthand property sets most of the outline properties in a single declaration.
+	 *
+	 * @param color an instance of the Hex class
+	 * @param weight the thickness of the stroke in pixels
+	 * @param position the position of the stroke relative to the bounding box
+	 * @param style either solid or dashed
+	 */
+	public constructor(color: Hex, weight = 1, position: 'center' | 'inside' | 'outside' = 'outside', style: 'dash' | 'solid' = 'solid') {
+		assertIsInstanceOfHex(color);
+		this.#color = color;
+		assertIsValidPosition(position);
+		this.#position = position;
+		assertIsValidStyle(style);
+		this.#style = style;
+		assertIsNonNegative(weight);
+		this.#weight = weight;
+	}
+
+	/**
+	 * Returns the outline offset as a 'Npx' string
+	 *
+	 * @returns string
+	 */
+	public toStyleOffsetString(): string {
+		if (this.#position === 'center') {
+			return `-${this.#weight * 0.5}px`;
+		}
+		if (this.#position === 'inside') {
+			return `-${this.#weight}px`;
+		}
+		return '0px';
+	}
+
+	/**
+	 * Return the Stroke value as a 'Npx style #RRGGBBAA' string
+	 *
+	 * @returns string
+	 */
+	public toStyleString(): string {
+		if (this.#style === 'solid') {
+			return `${this.#weight}px solid ${this.#color.toStyleString()}`;
+		}
+		return `${this.#weight}px dashed ${this.#color.toStyleString()}`;
+	}
+}
+
+function assertIsInstanceOfHex(value: unknown): asserts value is Hex {
+	if (value instanceof Hex) {
+		return;
+	}
+	throw new TypeError(`[${value}] is invalid, must be an instance of Hex`);
+}
+
+function assertIsNonNegative(value: unknown): asserts value is number {
+	if (typeof value === 'number' && value >= 0) {
+		return;
+	}
+	throw new RangeError(`[${value}] is invalid, must be a non negative number`);
+}
+
+function assertIsValidPosition(value: unknown): asserts value is 'center' | 'inside' | 'outside' {
+	if (value === 'center' || value === 'inside' || value === 'outside') {
+		return;
+	}
+	throw new TypeError(`[${value}] is invalid, must be "center", "inside" or "outside"`);
+}
+
+function assertIsValidStyle(value: unknown): asserts value is 'dash' | 'solid' {
+	if (value === 'solid' || value === 'dash') {
+		return;
+	}
+	throw new TypeError(`[${value}] is invalid, must be "solid" or "dash"`);
+}
